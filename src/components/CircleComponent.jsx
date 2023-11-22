1cf2b27a2a1b1398197f83ffb8c8462b9e180665
dDangel <sanekgme@gmail.com>
2023-11-23 01:56:27 +0500

    info board

--- src/components/CircleComponent.jsx
+++ src/components/CircleComponent.jsx
@@ -10,11 +10,6 @@ export const CircleComponent = () => {
 				<LockSvgComponent outerRadius={80} innerRadius={70} segmentsCount={32} />
 				<LockSvgComponent outerRadius={60} innerRadius={50} segmentsCount={32} />
 			</div>
-			<div className={styles.anyContainer}>
-				<LockSvgComponent outerRadius={100} innerRadius={90} segmentsCount={32} />
-				<LockSvgComponent outerRadius={80} innerRadius={70} segmentsCount={32} />
-				<LockSvgComponent outerRadius={60} innerRadius={50} segmentsCount={32} />
-			</div>
 		</div>
 	)
 }
--- src/pages/board/Board.jsx
+++ src/pages/board/Board.jsx
@@ -5,12 +5,32 @@ import { CircleComponent } from "../../components/CircleComponent.jsx";
 export const Board = () => {
 
 	return (
-		<div>
+		<div className={styles.gameField}>
 			<div className={styles.gameBoard}>
 				<CircleComponent/>
 			</div>
 			<div className={styles.infoBoard}>
-
+				<div className={styles.topBarMenu}>
+					<div className="nameLocker">
+						DIGPICK LINKED<br/>
+						DOOR 
+					</div>
+					<div className="lvlLocker">
+						SECURITY LEVEL <br/>
+						EXPERT
+					</div>
+					<div className={styles.selectingComb}>
+						<div className="topCountComb">
+							<div className="topCountCombName">DIGIPICKS</div>
+							<div className="topCountCombCount">7</div>
+						</div>
+						<div className="changeComb">
+							<div className="comb1"></div>
+							<div className="comb2"></div>
+						</div>
+					</div>
+				</div>
+				<div className="botBarKey">qwerty</div>
 			</div>
 		</div>
 	)
