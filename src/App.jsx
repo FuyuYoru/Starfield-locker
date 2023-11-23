f12b9360f47782fb40d3117ace924dab458272aa
vveli <vvelichko.2000@mail.ru>
2023-11-23 22:57:07 +0500

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Home } from "./pages/home/Home.jsx";
 import { Board } from "./pages/board/Board.jsx";
+import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 export const App = () => {
 	const [activePage, setActivePage] = useState('Home');
 
 	return (
-		activePage === 'Home'? <Home onGameStart={() => setActivePage('Board')}/>
-		: <Board onGameEnd={() => setActivePage('Home')}/>
+		<>
+			<Router>
+				<Routes>
+					<Route path='/' element={<Home />}>
+					</Route>
+					<Route path='/game' element={<Board />}>
+					</Route>
+				</Routes>
+			</Router>
+		</>
 	)
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from './Home.module.css'
+import { NavLink } from "react-router-dom";
 
 export const Home = () => {
 	return (
 		<div className={styles.HomeContainer}>
 			<h1>Lorem, ipsum dolor.</h1>
 			<div> Slider container</div>
-			<button>Start</button>
+			<NavLink to={"game"}>
+				<button>Start</button>
+			</NavLink>
 		</div>
 	)
 };
